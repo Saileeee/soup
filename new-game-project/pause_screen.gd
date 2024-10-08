c6629c714eb1f83339076d0135860204e84777f3
Saileeee <saileea.varde@gmail.com>
2024-10-08 09:13:20 -0400

    ahhhhhh pause

--- new-game-project/pause_screen.gd
+++ new-game-project/pause_screen.gd
@@ -1,11 +1,15 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void: #can still move when paused
+	if Input.is_action_just_pressed("pause"):
+		if visible:
+			hide()
+		else:
+			show()
+			$paused_camera.make_current()
