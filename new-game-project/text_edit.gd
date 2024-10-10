700312aebafeb294e0079f2f4e3d9db6bd99f853
Saileeee <saileea.varde@gmail.com>
2024-10-10 09:27:58 -0400

    hi

--- new-game-project/text_edit.gd
+++ new-game-project/text_edit.gd
@@ -1,9 +1,9 @@
-extends Label
+extends TextEdit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	hide()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
