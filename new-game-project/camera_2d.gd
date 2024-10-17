1c8b1e17ff6cd649aeb4047a227115077f6519d7
Saileeee <saileea.varde@gmail.com>
2024-10-17 08:53:44 -0400

    hmmmmmmm :(

--- new-game-project/camera_2d.gd
+++ new-game-project/camera_2d.gd
@@ -1,11 +1,12 @@
 extends Camera2D
-var current_room = "room1"
+var current_room
 var dimensions1 = [0, 1152, -84, 640]
-var dimensions2 = [1186, 2338, -84, 640]
+var dimensions2 = [1152, 2304, -84, 640]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#these values are based off the room size, should make them clearer
+	current_room = "room1"
 	limit_left = dimensions1[0]
 	limit_right = dimensions1[1]
 	limit_top = dimensions1[2]
@@ -13,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(position)
 
 
 func _on_pause_screen_hidden() -> void:
@@ -25,6 +26,7 @@ func _on_welcome_hidden() -> void:
 
 
 func _on_room_1_area_body_entered(body: Node2D) -> void:
+	print("room1 entered")
 	current_room = "room2"
 	limit_left = dimensions2[0]
 	limit_right = dimensions2[1]
@@ -33,6 +35,7 @@ func _on_room_1_area_body_entered(body: Node2D) -> void:
 
 
 func _on_room_2_area_body_entered(body: Node2D) -> void:
+	print("room2 entered")
 	current_room = "room1"
 	limit_left = dimensions1[0]
 	limit_right = dimensions1[1]
