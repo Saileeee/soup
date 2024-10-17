c195aae43a50f4b0df8cf96514d08e0b2a0fa97a
eazybriezy <142619338+eazybriezy@users.noreply.github.com>
2024-10-17 09:09:11 -0400

    time sucks i hate time

--- new-game-project/timer.gd
+++ new-game-project/timer.gd
@@ -10,10 +10,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = a
+	pass
 
 
 
 func _on_timeout() -> void:
-	a += 1
+	pass
+	
 	 
