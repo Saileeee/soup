365fee360d150274b612030f47888f13044b92af
Saileeee <saileea.varde@gmail.com>
2024-10-17 08:43:00 -0400

    fixing camera

--- new-game-project/screen_camera.gd
+++ new-game-project/screen_camera.gd
@@ -1,11 +1,17 @@
 extends Camera2D
-
+var paused
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	make_current()
-
+	paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_pause_screen_visibility_changed() -> void:
+	paused = not paused
+	if paused:
+		make_current()
