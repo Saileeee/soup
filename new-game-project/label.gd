bed3e41ce3d19f0c2680e7a2bdc4231fb30f9e82
eazybriezy <142619338+eazybriezy@users.noreply.github.com>
2024-10-17 10:02:24 -0400

    the meaningless eternity of the universe crashes down upon us

--- new-game-project/label.gd
+++ new-game-project/label.gd
@@ -1,15 +1,23 @@
-extends Timer
+extends Label
 
 var a = 0
+var c = 6
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	wait_time = 2
-	autostart = true# Replace with function body.
+	text = "6:00"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	 
+
+
+func _on_timer_timeout() -> void:
+	a+=10
+	if a==60:
+		a=0
+		c+=1
+		text = str(c)+":0"+str(a)
+	else:
+		text = str(c)+":"+str(a)
