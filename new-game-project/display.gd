7ea8bcb32378483a27099750274e42863150ec21
Saileeee <saileea.varde@gmail.com>
2024-10-23 09:40:04 -0400

    microwaveeeeee :( stop and start buttons

--- new-game-project/display.gd
+++ new-game-project/display.gd
@@ -1,28 +1,48 @@
 extends Label
-var timer
-var time: String
+var timeStr: String
+var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
 
-func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
-	if index == 10: #stop
-		pass
-	elif time.length()>4: #max char limit
-		return
-	elif index <= 8: #1-9
-		time += str(index+1)
-	elif index == 10: #0
-		time += str(0)
-	elif index == 11: #start
-		timer = timer.new()
-		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if $Timer.time_left == 0:
+		time = 0
+		$Timer.stop()
+	if time != 0:
+		text = "0".repeat(4-timeStr.length())+str(int($Timer.time_left))
+		text = text.insert(2, ":")
+	
+
+func _on_item_list_item_clicked(index: int, _at_position: Vector2, _mouse_button_index: int) -> void:
+	if time == 0: #no timer running
+		if index == 9: #stop
+			timeStr = ""
+		elif timeStr.length()>=4: #max char limit
+			return
+		elif index <= 8: #1-9
+			timeStr += str(index+1)
+		elif index == 10: #0
+			timeStr += str(0)
+		elif index == 11: #start
+			#				seconds					hours
+			time = int(text.get_slice(":",1)) + int(text.get_slice(":",0))*60
+			print(text.get_slice(":",1).is_valid_int())
+			print(text.get_slice(":",1))
+			print(int(text.get_slice(":",1)))
+			$Timer.wait_time = time
+			$Timer.start()
+			timeStr = ""
+		text = "0".repeat(4-timeStr.length())+str(timeStr)
+		text = text.insert(2, ":")
+	if $Timer.is_paused():
+		if index == 9: #timer running, stop pressed
+			timeStr = ""
+		elif index == 11:
+			$Timer.set_paused(false)
 	else:
-		text = time.insert(2,":")
+		if index == 9:
+			$Timer.set_paused(true)
