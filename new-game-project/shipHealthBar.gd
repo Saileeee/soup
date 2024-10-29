b1794175b832e4caa542b999973adf96ddc5b5b3
Avalyn <almarraro@gmail.com>
2024-10-29 08:14:16 -0400

    a

---
+++ new-game-project/shipHealthBar.gd
@@ -0,0 +1,10 @@
+extends Label
+
+var health = 100
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = "Ship Health: "+health+"%"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
