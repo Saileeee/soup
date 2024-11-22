0c7fcbe5219e15c903d137c77b7d69e821c71e10
eazybriezy <142619338+eazybriezy@users.noreply.github.com>
2024-11-22 08:43:59 -0500

    Door stuff plus building

--- new-game-project/crowman.gd
+++ new-game-project/crowman.gd
@@ -7,7 +7,7 @@ var room_move = 0
 var entered_room = false
 
 func _physics_process(_delta: float) -> void:
-	if start:
+	##if start:
 		var x_direction = Input.get_axis("move_left", "move_right")
 		velocity.x = x_direction
 		var y_direction = Input.get_axis("move_up", "move_down") #up is negative and down is positive
