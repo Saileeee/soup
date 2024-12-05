05f8f572027a7c1014ef011bd54b39cee8bae47e
Saileeee <saileea.varde@gmail.com>
2024-12-05 09:19:50 -0500

    blargh

--- new-game-project/Characters/crowman.gd
+++ new-game-project/Characters/crowman.gd
@@ -6,9 +6,11 @@ var speaking = false
 var entered_room = false
 const SPEED = 300.0
 var room_move = 0
+var room_move_timer = Timer.new()
 
 func _ready() -> void:
 	position = Vector2(-1726,461)
+	room_move_timer.wait_time = 0.3
 	
 func _physics_process(_delta: float) -> void:
 	if started and not speaking: 
@@ -27,7 +29,7 @@ func _physics_process(_delta: float) -> void:
 			$AnimatedSprite2D.stop()
 			
 		if entered_room:
-			$"room move timer".start()
+			room_move_timer.start()
 			entered_room = false
 		if room_move != 0:
 			velocity.x = room_move
