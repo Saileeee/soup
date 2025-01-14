3b9d646ef9b561d4b1c24bb2c8308adae7c8e1a5
Saileeee <saileea.varde@gmail.com>
2025-01-14 09:39:58 -0500

    bhar for easier debug

--- new-game-project/animation_player.gd
+++ new-game-project/animation_player.gd
@@ -1,9 +1,9 @@
-extends TextureButton
+extends AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	play("Art/purple window highlight")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
