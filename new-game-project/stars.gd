52c2b53dc4b6a543e792bbcfdca6051e77796b5e
Saileeee <saileea.varde@gmail.com>
2025-01-21 11:26:04 -0500

    finished task but can still move

--- new-game-project/stars.gd
+++ new-game-project/stars.gd
@@ -1,45 +1,50 @@
-extends TextureButton
+extends Node2D
 var in_range = []
-var char_RIDs = [] #crowman, stickman, fly
-var currRID
+var chars = [] #crowman, stickman, fly
+var currChar
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	char_RIDs.append(get_node("/Node2D/characters/crowman").get_id())
-	char_RIDs.append(get_node("/Node2D/characters/stickman").get_id())
-	char_RIDs.append(get_node("/Node2D/characters/fly").get_id())
+	chars.append(get_node("../characters/crowman"))
+	chars.append(get_node("../characters/stickman"))
+	chars.append(get_node("../characters/fly"))
+	$button.hide()
+	currChar = chars[0]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if currRID in in_range:
-		show()
+	count += 1
+	if count >= 100:
+		count = 0
+		print("currChar in range: ",str(currChar in in_range))
+	if currChar in in_range:
+		$button.show()
 	else:
-		hide()
+		$button.hide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(str(body)," entered")
+	in_range.append(body)
 
 
-
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	for rid in char_RIDs:
-		if rid == body_rid:
-			in_range.append(body_rid)
-
-
-func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	for i in range(char_RIDs.size()): #goes backwards because removing from the array
-		if char_RIDs[i] == body_rid:
-			in_range.erase(body_rid)
-
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print(str(body)," exited")
+	in_range.erase(body)
 
 func _on_task_ui_crowman_time() -> void:
-	currRID = char_RIDs[0]
+	print("currChar: crowman")
+	currChar = chars[0]
 
 func _on_task_ui_stickman_time() -> void:
-	currRID = char_RIDs[1]
+	print("currChar: stickman")
+	currChar = chars[1]
 
 func _on_task_ui_fly_time() -> void:
-	currRID = char_RIDs[2]
+	"currChar: fly"
+	currChar = chars[2]
 
-
-func _on_pressed() -> void:
-	if currRID in in_range:
-		get_node("tasks/star task").show()
+func _on_texture_button_pressed() -> void:
+	if currChar in in_range:
+		get_node("../tasks/star task").show()
+		print(get_node("../tasks/star task")," shown")
--- new-game-project/tasks.gd
+++ new-game-project/tasks.gd
@@ -1,5 +1,5 @@
-extends CanvasLayer
-
+extends CanvasLayer #-414
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	count += 1
+	if count >= 100:
+		count = 0
+		print("star task visible: ",$"star task".visible)
+
+func _on_star_close_pressed() -> void:
+	$"star task".hide()
