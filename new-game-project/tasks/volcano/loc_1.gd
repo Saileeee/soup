cf7d8b8768f294fca78568ece7dc40d1cc40a475
Sailee <saileea.varde@gmail.com>
2025-03-11 09:41:30 -0400

    I don't understandddddddddddd drag and drop stuff

---
+++ new-game-project/tasks/volcano/loc_1.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+	return false
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	data.get_parent().remove_child(data)
+	add_child(data)
--- new-game-project/tasks/volcano/obj_1.gd
+++ new-game-project/tasks/volcano/obj_1.gd
@@ -1,1 +1,11 @@
 extends TextureRect
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+	var data = 1
+		
+	#set drag preview, eventually change to move the actual object
+	var preview = TextureRect.new()
+	preview.texture = texture
+	set_drag_preview(preview)
+	
+	return self
