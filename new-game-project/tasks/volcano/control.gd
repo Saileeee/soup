af51084c4c1cc47fbc2c1098193aedd9f31feaae
Saileeee <saileea.varde@gmail.com>
2025-03-12 09:41:40 -0400

    hiiiiii dragging stuff

--- new-game-project/tasks/volcano/control.gd
+++ new-game-project/tasks/volcano/control.gd
@@ -2,8 +2,6 @@ extends Control
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	return true
-	return false
-	
+
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	data.get_parent().remove_child(data)
-	add_child(data)
+	data.position = at_position-Vector2(data.texture.region.size/2)
