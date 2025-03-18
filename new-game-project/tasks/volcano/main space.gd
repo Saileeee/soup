2221681549801e5f61904495522f74cdf49fe33f
Saileeee <saileea.varde@gmail.com>
2025-03-18 09:37:26 -0400

    jigsaw code works

--- new-game-project/tasks/volcano/main space.gd
+++ new-game-project/tasks/volcano/main space.gd
@@ -3,15 +3,22 @@ extends Control
 var num_in_place = 0
 var count = 0
 
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
 func _process(delta: float) -> void:
-	count += 1
-	if count > 100:
-		count = 0
-		print(num_in_place)
+	$TextureRect.texture = $AnimatedSprite2D.get_sprite_frames().get_frame_texture("default", $AnimatedSprite2D.frame) #set texture to current frame of animation
+	#count += 1
+	#if count > 100:
+		#count = 0
+		#print(num_in_place)
+	if num_in_place == 10:
+		$"full image".show()
+		get_tree().call_group("pieces", "hide")
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	if data.is_in_position:
-		return false
+	#if data.is_in_position:
+		#return false
 	return true
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
--- new-game-project/tasks/volcano/piece.gd
+++ new-game-project/tasks/volcano/piece.gd
@@ -3,19 +3,23 @@ extends TextureRect
 var is_dragging = false
 var is_in_position = false
 @export var goal: Vector2
-@onready var root = get_node("/root") #runs an error
+@onready var root = get_node("/root/Volcano") #runs an error
 
 func _process(delta: float) -> void:
 	if is_dragging and not is_in_position:
 		hide()
+		top_level = true
 		if Input.is_action_just_released("click"):
 			show()
 			is_dragging = false
+			top_level = false
 			if goal.distance_to(position) < 50:
 				print(goal.distance_to(position))
 				is_in_position = true
+				z_index = -1
 				position = goal
 				root.num_in_place += 1
+				mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	if not is_in_position:
